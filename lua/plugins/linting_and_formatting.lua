f84fcb26acf302317503bac9a12120543ac5ac1a
Leozin <leolbpl143@gmail.com>
2025-08-27 07:39:40 -0300

    setting up

---
+++ lua/plugins/linting_and_formatting.lua
@@ -0,0 +1,16 @@
+return {
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+      },
+    },
+  },
+}
---
+++ lua/plugins/old_config_plugins.lua
@@ -0,0 +1,234 @@
+return {
+  -- colorscheme
+  {
+    "drewtempelmeyer/palenight.vim",
+    lazy = false, -- Load always
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- Load always
+  },
+
+  -- git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  -- Tabnine (commented out in original, so keeping it commented)
+  -- { "codota/tabnine-nvim", build = "./dl_binaries.sh" },
+
+  {
+    "akinsho/bufferline.nvim",
+    -- tag = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
+
+  -- surround vim
+  "tpope/vim-surround",
+
+  -- nerd commenter
+  "scrooloose/nerdcommenter",
+
+  -- status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+  },
+
+  -- show recent files on empty nvim command
+  "mhinz/vim-startify",
+
+  -- lsp config
+  {
+    "williamboman/mason.nvim",
+    -- "williamboman/mason-lspconfig.nvim", -- LazyVim handles this
+    -- "neovim/nvim-lspconfig", -- LazyVim handles this
+  },
+
+
+
+  -- GitHub Copilot
+  -- { "github/copilot.vim" },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "github/copilot.vim",
+      "nvim-lua/plenary.nvim",
+    },
+    build = "make tiktoken",
+    config = function()
+      require("CopilotChat").setup({
+        window = {
+          layout = "float",
+          relative = "editor",
+          anchor = "NE",
+          border = "rounded",
+          width = 0.3,
+          height = 0.6,
+          row = 1,
+          col = vim.o.columns - 1,
+        },
+      })
+    end,
+  },
+
+  --[[ Codeium (commented out in original, so keeping it commented)
+  {
+    "Exafunction/codeium.vim",
+    config = function ()
+      vim.keymap.set("i", "<C-g>", function () return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+    end,
+  },
+  ]]
+
+  {
+    "kkoomen/vim-doge",
+    build = ":call doge#install()",
+  },
+
+  -- Lua
+  {
+    "abecodes/tabout.nvim",
+    config = function()
+      require("tabout").setup {
+        tabkey = "<Tab>",
+        backwards_tabkey = "<S-Tab>",
+        act_as_tab = true,
+        act_as_shift_tab = false,
+        default_tab = "<C-t>",
+        default_shift_tab = "<C-d>",
+        enable_backwards = true,
+        completion = true,
+        tabouts = {
+          {open = "'", close = "'"},
+          {open = "\"", close = "\""},
+          {open = "`", close = "`"},
+          {open = "(", close = ")"},
+          {open = "[", close = "]"},
+          {open = "{", close = "}"}
+        },
+        ignore_beginning = true,
+        exclude = {}
+      }
+    end,
+    dependencies = {"nvim-treesitter"}, -- Use dependencies instead of wants
+    after = {"nvim-cmp"}, -- Use after instead of after
+  },
+
+  -- this will enable LSP for neovim config
+  "folke/neodev.nvim",
+
+  -- for LSP autocompletion
+
+
+  -- telescope - searching / navigation
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    config = function()
+      -- Carregar a extensão de live_grep_args
+      require("telescope").load_extension("live_grep_args")
+
+      -- Mapeamento para usar find_files para mostrar todos os arquivos (não só do git)
+      vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
+    end,
+  },
+
+
+
+  -- better highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = "kyazdani42/nvim-web-devicons",
+    config = function() require("nvim-tree").setup {} end,
+  },
+
+  {
+    "amrbashir/nvim-docs-view",
+    opt = true,
+    cmd = { "DocsViewToggle" },
+    config = function()
+      require("docs-view").setup {
+        position = "bottom",
+        width = 60,
+      }
+    end,
+  },
+
+  -- prettier tabs
+  "romgrk/barbar.nvim",
+
+
+
+  -- nice diagnostic pane on the bottom
+  "folke/lsp-trouble.nvim",
+
+  -- better LSP UI (for code actions, rename etc.)
+  "tami5/lspsaga.nvim",
+
+  -- show indentation levels
+  "lukas-reineke/indent-blankline.nvim",
+
+  -- highlight variables under cursor
+  "RRethy/vim-illuminate",
+
+  "rcarriga/nvim-notify",
+
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup {
+        filetypes = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+      }
+    end,
+  },
+
+  -- conform.nvim (already handled in linting_and_formatting.lua)
+  -- { "stevearc/conform.nvim", config = function() require("conform").setup() end, },
+
+  "iamcco/markdown-preview.nvim",
+
+  "NvChad/nvim-colorizer.lua",
+
+  "folke/zen-mode.nvim",
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
